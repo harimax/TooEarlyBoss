645a78cbb175bda9babb810b2c3284a22c024b28
harimax <yu07mario9@gmail.com>
2025-04-06 22:34:31 +0900

    選択画面でのUIの仕組みを作成
    ボタンを押下することでパラメータが上昇

---
+++ Assets/Script/TrianingButton.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TrianingButton : MonoBehaviour
+{
+    public float PlayerPower = 1f;
+    public float PlayerHealth = 1f;
+    public float PlayerStamina = 1f;
+    public float PlayerLucky = 1f;
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI staminaText;
+    [SerializeField] private TextMeshProUGUI luckyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //パワーボタンを押下してパワーがアップ
+    public void TrainingAttack()
+    {
+        PlayerPower += increaceParameter(1, 5);
+        UpdateUI();
+    }
+    //体力ボタンを押下して体力がアップ
+    public void TrainingHealth()
+    {
+        PlayerHealth += increaceParameter(1, 5);
+        UpdateUI();
+    }
+
+    //スタミナボタンを押下してスタミナがアップ
+    public void TrainingStamina()
+    {
+        PlayerStamina += increaceParameter(1, 5);
+        UpdateUI();
+    }
+    //ラッキーボタンを押下してラッキーがアップ
+    public void TrainingLucky()
+    {
+        PlayerLucky += increaceParameter(1, 5);
+        UpdateUI();
+    }
+
+    //上昇値を決めるメソッド
+    private int increaceParameter(int minIncrease, int maxIncrease)
+    {
+        return Random.Range(minIncrease, maxIncrease);
+    }
+    //UIの更新
+    void UpdateUI()
+    {
+        attackText.text = PlayerPower.ToString();
+        healthText.text = PlayerHealth.ToString();
+        staminaText.text = PlayerStamina.ToString();
+        luckyText.text = PlayerLucky.ToString();
+    }
+}
