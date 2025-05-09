05d585537cee2b7992bf6d13e0a7366ad07b5630
harimax <yu07mario9@gmail.com>
2025-05-10 02:52:49 +0900

    コード整形とターン数応じてボタンを有効化無効化するように変更

--- Assets/Script/UI/TrianingButton.cs
+++ Assets/Script/UI/TrianingButton.cs
@@ -6,37 +6,43 @@ using TMPro;
 public class TrianingButton : MonoBehaviour
 {
     public float PlayerPower = 1f;
-    public float PlayerHealth = 1f;
+    public int PlayerHealth = 1;
     public float PlayerStamina = 1f;
     public float PlayerSpecial = 1f;
-    public int turnNumber=0;
+    public static int turnNumber = 0;
     [SerializeField] private TextMeshProUGUI attackText;
     [SerializeField] private TextMeshProUGUI healthText;
     [SerializeField] private TextMeshProUGUI staminaText;
     [SerializeField] private TextMeshProUGUI specialText;
     [SerializeField] private TextMeshProUGUI turn;
+    [SerializeField] private int minIncrease = 1;
+    [SerializeField] private int maxIncrease = 5;
+    [SerializeField] private List<UnityEngine.UI.Button> trainingButtons;
     // Start is called before the first frame update
     void Start()
     {
+        turnNumber = 19;
+        // 最初はボタンを有効化
         UpdateUI();
     }
 
     // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-
+        // Debug.Log("オブジェクトがアクティブ化されました！");
+        UpdateUI();
     }
     //パワーボタンを押下してパワーがアップ
     public void TrainingAttack()
     {
-        PlayerPower += increaceParameter(1, 5);
+        PlayerPower += increaceParameter(minIncrease, maxIncrease);
         turnNumber--;
         UpdateUI();
     }
     //体力ボタンを押下して体力がアップ
     public void TrainingHealth()
     {
-        PlayerHealth += increaceParameter(1, 5);
+        PlayerHealth += increaceParameter(minIncrease, maxIncrease);
         turnNumber--;
         UpdateUI();
     }
@@ -44,14 +50,14 @@ public class TrianingButton : MonoBehaviour
     //スタミナボタンを押下してスタミナがアップ
     public void TrainingStamina()
     {
-        PlayerStamina += increaceParameter(1, 5);
+        PlayerStamina += increaceParameter(minIncrease, maxIncrease);
         turnNumber--;
         UpdateUI();
     }
     //ラッキーボタンを押下してラッキーがアップ
     public void TrainingSpcial()
     {
-        PlayerSpecial += increaceParameter(1, 5);
+        PlayerSpecial += increaceParameter(minIncrease, maxIncrease);
         turnNumber--;
         UpdateUI();
     }
@@ -62,12 +68,24 @@ public class TrianingButton : MonoBehaviour
         return Random.Range(minIncrease, maxIncrease);
     }
     //UIの更新
-    void UpdateUI()
+    public void UpdateUI()
     {
         attackText.text = PlayerPower.ToString();
         healthText.text = PlayerHealth.ToString();
         staminaText.text = PlayerStamina.ToString();
         specialText.text = PlayerSpecial.ToString();
-        turn.text=$"残り: {turnNumber}ターン";
+        turn.text = $"残り: {turnNumber}ターン";
+
+        SetButtonsInteractable();
+    }
+    // ターン数に応じてボタンを有効/無効にするメソッド
+    public void SetButtonsInteractable()
+    {
+        bool canTrain = (turnNumber % 5 != 0) && (turnNumber > 0);
+
+        foreach (var button in trainingButtons)
+        {
+            button.interactable = canTrain;
+        }
     }
 }
