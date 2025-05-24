afbac90c74631d91dec987f2b9a9dabe0e758211
harimax <yu07mario9@gmail.com>
2025-05-24 21:03:41 +0900

    スキルのscriptableObjectを実装

---
+++ Assets/Script/SkillScript/SkillBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SkillBase : ScriptableObject
+{
+    public string skillName;
+    public string description;
+
+    //条件を満たされたときに効果を適応させる
+    public abstract void ApplyEffect(GameObject player);
+    public virtual void UpdateConditional(GameObject player) {}
+
+}
